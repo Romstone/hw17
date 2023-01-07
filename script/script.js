4f4aa80b96a75132cc8e0fea161fe64137373602
Romstone <czeslawmeyerx@gmail.com>
2023-01-08 01:48:42 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -1,34 +1,79 @@
-const outputDiv = document.getElementById("output");
-const storage = localStorage.getItem("tickets");
-const ticketsArr = (storage) ? JSON.parse(storage) : [];
+//elements
+    const destCity = document.getElementById('dest_city');
+    const destCountry = document.getElementById('dest_country');
+    const budget = document.getElementById('budget_value');
+    const startDate = document.getElementById('start_date');
+    const endDate = document.getElementById('end_date');
+    const persons = document.getElementById('persons');
+    const vehicle = document.getElementById('transfer_type');
+    const outputSpan = document.getElementById('output');
+    let ticket;
 
-function addTicket() {
-    const ticket = {
-        origin: document.getElementById("origin").value,
-        destination: document.getElementById("destination").value,
-        budget: document.getElementById("budget").value,
-        start: document.getElementById("start_date").value,
-        end: document.getElementById("end_date").value,
-        persons: document.getElementById("persons").value,
-        vehicle: document.getElementById("vehicle").value
+//database init
+    const storage = localStorage.getItem("tickets");
+    const ticketsArr = (storage) ? JSON.parse(storage) : [];
+
+    const createTicket = () => {
+        if (destCity.value === "") {
+            alert("Fill in 'City' field");
+            return false;
+        } else if (destCountry.value === "") {
+            alert("Fill in 'Country' field");
+            return false;
+        } else {
+            ticket = {
+                destCity: destCity.value,
+                destCountry: destCountry.value,
+                budget: budget.value,
+                startDate: startDate.value,
+                endDate: endDate.value,
+                persons: persons.value,
+                vehicle: vehicle.value
+            }
+            return true;
+        }
     }
-    ticketsArr.push(ticket);
-    localStorage.setItem("tickets", JSON.stringify(ticketsArr));
-    outputDiv.innerHTML = showTickets(ticketsArr);
-}
+    const save = () => {
+        if (createTicket()) {
+            ticketsArr.push(ticket);
+            localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+        } else
+            return -1;
+        render();
+    }
 
-const showTickets = (arr) => {
-    return ['<ul style="list-style-type: none">', ...arr.map((value, index) => {
-        return `<li class="ticket_card" record_id="${index}"
-        <h2>From ${value.origin} to ${value.destination}</h2>
-        <div style="">
-            <img src="/img/edit-icon.png" alt="edit" width="16">
-            <img src="/img/delete-icon.png" alt="delete" width="16">
-            <img src="/img/dots-icon.png" alt="menu" width="16">
-        </div>
-        <h5>Expected budget: ${value.budget} ILS</h5>
-        <h5>${value.start} - ${value.end} | ${value.persons} persons | ${value.vehicle}</h5>
-      </li>`;
-    }), '</ul>'].join('');
-}
+    const render = () => {
+        const printTicket = ticketsArr.map((value, index) => `
+            <li class="ticket_card" index="${index}">
+                <div class="card travel-card">
+                    <div class="title">
+                        <h4>From Haifa to ${value.destCity}</h4>
+                        <i class="bi bi-pencil-square edit" index="${index}"></i>
+                        <i class="bi bi-x-circle remove" index="${index}"></i>
+                        <i class="bi bi-three-dots-vertical details" index="${index}"></i>
+                    </div>
+                    <span>Expected budget: ${value.budget} ILS</span>
+                    <span>${value.startDate} - ${value.endDate} | 
+                        ${value.persons} persons | ${value.vehicle}</span>
+                <div>
+            </li>
+    `).join('');
+        outputSpan.innerHTML = printTicket;
 
+        document.querySelectorAll('.remove').forEach((value) => {
+            value.addEventListener('click', () => {
+                const id = Number(value.getAttribute('index'));
+                ticketsArr.splice(id, 1);
+                localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+                render();
+            });
+        });
+
+        document.querySelectorAll('.details').forEach((value) => {
+            value.addEventListener('click', () => {
+                const id = Number(value.getAttribute('index'));
+                alert(`Ticket ${id + 1}`);
+            })
+        });
+    }
+    render();
